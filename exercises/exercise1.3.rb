ad5ab702754a301f34c29c7b1637da529e1f6e3c
MichaelAI0 <99698667+MichaelAl0@users.noreply.github.com>
2023-01-11 11:38:44 -0600

    finish exercise1.3

---
+++ exercises/exercise1.3.rb
@@ -0,0 +1,17 @@
+# puts "Please type a User Input:"
+# user_input = gets.chomp
+# puts "You typed #{user_input}"
+
+puts "Please enter a number, you'd like to multiply by two:"
+user_input = gets.chomp
+
+multi = user_input.to_i * 2
+
+puts "#{user_input} multiplied by 2 equals #{multi}!"
+
+puts "Please enter a number, you'd like to divide by two:"
+user_input2 = gets.chomp
+
+divi = user_input2.to_i / 2
+
+puts "#{user_input2} divided by 2 equals #{divi}!"
