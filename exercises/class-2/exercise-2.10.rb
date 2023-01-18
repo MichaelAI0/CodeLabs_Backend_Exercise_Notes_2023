fc7c6b6f1d7fea9a4a28c6a77ff97f53efbdea0b
MichaelAI0 <99698667+MichaelAl0@users.noreply.github.com>
2023-01-18 00:20:12 -0600

    update list

--- exercises/class-2/exercise-2.10.rb
+++ exercises/class-2/exercise-2.10.rb
@@ -1,1 +1,20 @@
-#TODO:Finish lessons ASAP!
+class Calculator
+  def add(input_1, input_2)
+    input_1 + input_2
+  end
+  def subtract(input_1, input_2)
+    input_1 - input_2
+  end
+  def divide(input_1, input_2)
+    input_1 / input_2
+  end
+  def multiply(input_1, input_2)
+    input_1 * input_2
+  end
+end
+
+calc = Calculator.new
+puts calc.add(1, 5)
+puts calc.subtract(5, 1)
+puts calc.divide(5, 1)
+puts calc.multiply(5, 1)
